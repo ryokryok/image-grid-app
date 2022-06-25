b92fc45213ad9dea7d6ae3d904a588d11514616e
mr_ozin <mrozin.dev@gmail.com>
2022-06-26 04:39:03 +0900

    feature popup to confirm config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, MouseEvent } from "react";
 import goat from "./goat-square-profile.jpg";
 import {
   A4,
@@ -5,6 +6,7 @@ import {
   generatePosition,
   defaultImageSize,
   Size2D,
+  Coordinate2D,
 } from "./lib/utils";
 
 type PrintSheetProps = {
@@ -15,23 +17,77 @@ type PrintSheetProps = {
 
 function PrintSheet({ paperSize, imageSize, gap }: PrintSheetProps) {
   const { width, height } = paperSize;
+
   const imagePosition = generatePosition(paperSize, imageSize, gap);
+
+  const [open, setOpen] = useState(false);
+  const [postion, setPostion] = useState<Coordinate2D>({ x: 0, y: 0 });
+  const handler = (event: MouseEvent<SVGImageElement>) => {
+    setOpen(!open);
+    const { clientX, clientY } = event;
+    setPostion({
+      x: clientX,
+      y: clientY,
+    });
+  };
   return (
-    <svg
-      viewBox={`0 0 ${width} ${height}`}
-      xmlns="http://www.w3.org/2000/svg"
-      fill="white"
-    >
-      {imagePosition.map(({ x, y }) => (
-        <image
-          width={imageSize.width}
-          height={imageSize.height}
-          x={x}
-          y={y}
-          href={goat}
-        />
-      ))}
-    </svg>
+    <div>
+      {open ? (
+        <div
+          style={{
+            backgroundColor: "Highlight",
+            position: "absolute",
+            padding: "0.25rem 0.5rem",
+            top: postion.y + 10,
+            left: postion.x + 10,
+          }}
+        >
+          <form>
+            <div>
+              <label htmlFor="imageWidth">Width (mm) </label>
+              <input
+                type="number"
+                name="imageWidth"
+                id="imageWidth"
+                value={imageSize.width}
+              />
+            </div>
+            <div>
+              <label htmlFor="imageHeight"> Height (mm)</label>
+              <input
+                type="number"
+                name="imageHeight"
+                id="imageHeight"
+                value={imageSize.height}
+              />
+            </div>
+            <div>
+              <label htmlFor="imageGap">Gap (mm)</label>
+              <input type="number" name="imageGap" id="imageGap" value={gap} />
+            </div>
+          </form>
+        </div>
+      ) : (
+        <></>
+      )}
+      <svg
+        viewBox={`0 0 ${width} ${height}`}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="white"
+      >
+        {imagePosition.map(({ x, y }) => (
+          <image
+            key={crypto.randomUUID()}
+            width={imageSize.width}
+            height={imageSize.height}
+            x={x}
+            y={y}
+            href={goat}
+            onClick={handler}
+          />
+        ))}
+      </svg>
+    </div>
   );
 }
 
